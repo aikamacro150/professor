844126e0d958eafb93cc0af60a9a8ae459e74757
Administrador <Administrador@10.10.4.12>
2019-07-01 18:45:57 -0300

    NAO ACABADO

---
+++ professor/src/modelo/professor.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class professor {
+    private int codigo;
+private String nome;
+private String sobrenome;
+private String endereco;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNomea() {
+        return nome;
+    }
+
+    public void setNomea(String nomea) {
+        this.nome = nomea;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
